570c471d44a70838537c180d6cdfbb27f450e1f0
Administrator <Administrator@MS-20170511IAEE>
2017-07-24 18:18:28 +0800

    添加项目文件。

---
+++ MobileBone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MobileBone.Startup))]
+namespace MobileBone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
